0bb8f68e6cd6ae44ba8d5f6ebf4d884541d71e51
Lechonita <jessicarouillon@hotmail.com>
2024-03-11 13:17:12 +0100

    ca tourne

---
+++ obj/.deps/main.d
@@ -0,0 +1,17 @@
+obj/main.o: src/main.cpp src/../inc/Server.hpp inc/../inc/Utils.hpp \
+  inc/../inc/defines.hpp inc/../inc/Client.hpp inc/../inc/Server.hpp \
+  inc/../inc/Channel.hpp src/../inc/defines.hpp
+
+src/../inc/Server.hpp:
+
+inc/../inc/Utils.hpp:
+
+inc/../inc/defines.hpp:
+
+inc/../inc/Client.hpp:
+
+inc/../inc/Server.hpp:
+
+inc/../inc/Channel.hpp:
+
+src/../inc/defines.hpp:
