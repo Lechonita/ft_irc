694b1aa59bc48fa2743b156bbd2e76618f5a4e60
Lechonita <jessicarouillon@hotmail.com>
2024-03-13 12:21:14 +0100

    correction incorrect password + saut de ligne dans error message

--- src/pass_utils.cpp
+++ src/pass_utils.cpp
@@ -3,15 +3,9 @@
 bool	Commands::isValidPassword(const std::string& password, const Client& client, const Server& server)
 {
 	if (password != server.getPassword())
+	{
 		Utils::sendErrorMessage(ERR_PASSWDMISMATCH, client);
-	// {
-	// 	// 464 ERR_PASSWDMISMATCH
-	// 	if (send(client.getClientSocket(), ERR_PASSWDMISMATCH,  strlen(ERR_PASSWDMISMATCH), 0) == ERROR)
-	// 	{
-	// 		perror(PERR_SEND);
-	// 	}
-	// 	return (false);
-	// }
-
+		return (false);
+	}
 	return (true);
 }
