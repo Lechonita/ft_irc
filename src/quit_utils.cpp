5bd23c86b06b0f165fb921888f0a811b8b3f2a2b
Lechonita <jessicarouillon@hotmail.com>
2024-03-15 13:21:00 +0100

    push for merge

--- src/quit_utils.cpp
+++ src/quit_utils.cpp
@@ -3,14 +3,20 @@
 
 void	Utils::notifyQuitinChannels(const Client& client, const Server& server)
 {
+	printf("Arrivée dans 1/ notifyQuitinChannels\n");
+
 	std::map<std::string, Channel>::iterator	itChannels;
 	for (itChannels = server.getChannelMap().begin(); itChannels != server.getChannelMap().end(); ++itChannels)
 	{
+		printf("  Entrée dans la première boucle for\n");
 		std::vector<channelClient>::iterator	itClients;
 		for (itClients = itChannels->second.getChannelClients().begin(); itClients != itChannels->second.getChannelClients().end(); ++itClients)
 		{
-			if (itClients->client->getClientSocket() == client.getClientSocket())
+			printf("    Entrée dans la deuxième boucle for\n");
+			int		currentSocket = itClients->client->getClientSocket();
+			if (currentSocket == client.getClientSocket())
 			{
+				printf("      Entrée dans if\n");
 				Utils::sendQuitMessagetoChannels(client.getClientNickname(), itChannels->second);
 			}
 		}
@@ -21,8 +27,10 @@ void	Utils::notifyQuitinChannels(const Client& client, const Server& server)
 
 void	Utils::sendQuitMessagetoChannels(const std::string& nickname, const Channel channel)
 {
-	std::vector<channelClient>::iterator	it = channel.getChannelClients().begin();
-	for(; it != channel.getChannelClients().end(); ++it)
+	printf("Entrée dans sendQuitMessagetoChannels\n");
+
+	std::vector<channelClient>::iterator	it; // issue here
+	for(it = channel.getChannelClients().begin(); it != channel.getChannelClients().end(); ++it)
 	{
 		displayQuitChannelMessage(nickname, it->client->getClientSocket());
 	}
@@ -32,12 +40,16 @@ void	Utils::sendQuitMessagetoChannels(const std::string& nickname, const Channel
 
 static std::string		quitMessage(const std::string& nickname)
 {
+	printf("Entrée dans quitMessage\n");
+
 	return (nickname + LEFT_CHANNEL);
 }
 
 
 void	Utils::displayQuitChannelMessage(const std::string& nickname, const int receiverSocket)
 {
+	printf("Entrée dans displayQuitChannelMessage\n");
+
 	if(send(receiverSocket, quitMessage(nickname).c_str(), strlen(quitMessage(nickname).c_str()), 0) == ERROR)
 	{
 		std::perror(PERR_SEND);
